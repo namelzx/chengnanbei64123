0a2f8619ce81193c301edd53a52c83ef6c652bed
Your Name <you@example.com>
2019-05-13 01:15:04 +0800

    服务器地址更新与2019-5-13

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '39.108.101.229',
     // 数据库名
     'database'        => 'sql_8yueguihua_c',
     // 用户名
